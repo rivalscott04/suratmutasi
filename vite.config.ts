7d116b7e360704452eac1aaf4242bdf5108854d6
rivalscott04 <rival.biasrori@outlook.com>
2025-07-25 13:06:10 +0800

    update source map

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,9 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  build: {
+    sourcemap: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
